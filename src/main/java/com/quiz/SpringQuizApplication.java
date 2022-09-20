7bc1fe23d75bf3cbcf3911662bd012a404a136d8
YEONJUPC\강정빈 <kjbv0070@gmail.com>
2022-09-20 21:44:14 +0900

    Revert "20220920 강의자료"
    This reverts commit 17e94384312208d6e609ad5c7af9440fdf866ede.

--- src/main/java/com/quiz/SpringQuizApplication.java
+++ src/main/java/com/quiz/SpringQuizApplication.java
@@ -1,17 +1,11 @@
 package com.quiz;
 
-import javax.sql.DataSource;
-
-import org.apache.ibatis.session.SqlSessionFactory;
-import org.mybatis.spring.SqlSessionFactoryBean;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@MapperScan(basePackages = "com.quiz.*")		// mapper scan 추가!!!
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
 @SpringBootApplication
 public class SpringQuizApplication {
 
@@ -19,14 +13,4 @@ public class SpringQuizApplication {
 		SpringApplication.run(SpringQuizApplication.class, args);
 	}
 
-	@Bean
-	public SqlSessionFactory sqlSessionFactory(DataSource dataSource) throws Exception {
-		SqlSessionFactoryBean sessionFactory = new SqlSessionFactoryBean();
-		sessionFactory.setDataSource(dataSource);
-
-		Resource[] res = new PathMatchingResourcePatternResolver().getResources("classpath:mappers/*Mapper.xml");
-		sessionFactory.setMapperLocations(res);
-
-		return sessionFactory.getObject();
-	}
 }
